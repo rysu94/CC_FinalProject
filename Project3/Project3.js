04966d9b8da971059a9b1c16c751c133723ffb58
rysu94 <ryan.su94@gmail.com>
2017-12-18 14:52:07 -0500

    Add files via upload

--- Project3/Project3.js
+++ Project3/Project3.js
@@ -1,4 +1,17 @@
+/*
+Final Project: Painterly Pong
+By: Ryan Su
+
+This sketch has 2 players play a game of pong. As they play, the canvas
+will slowly become painted red & blue. By the end of the game, the player
+with the most canvas covered will win.
+*/
+
+//This handles the game mode of the sketch
 var gameMode = 0;
+
+var timer = 0;
+var adjustedTimer = 0;
 
 var P1_Paddle;
 var P2_Paddle;
@@ -8,9 +21,38 @@ var keys = [];
 var theBall;
 var ballX = [];
 var ballY = [];
+var ballSize = [];
 
 //1 = white, 2 = red, 3 = blue
 var ballColor = [];
+
+var pixelCounted = false;
+var redPixels = 0;
+var bluePixels = 0;
+
+var particles = [];
+var bullets = [];
+
+
+var P1_Wall;
+var P2_Wall;
+
+
+//Sound Variables
+var paddleNoise;
+var boomNoise;
+var backgroundNoise;
+var gameNoise;
+var gameEnd;
+
+function preload()
+{
+  paddleNoise = loadSound("Beep.ogg");
+  boomNoise = loadSound("boom1.ogg");
+  backgroundNoise = loadSound("8Bit_Theme.mp3");
+  gameNoise = loadSound("8Bit_TitleScreen.mp3");
+  gameEnd = loadSound("win.ogg");
+}
 
 function setup() 
 {
@@ -18,30 +60,162 @@ function setup()
   frameRate(60);
   SetPaddles();
   SetBall();
+  backgroundNoise.setLoop(true);
+  backgroundNoise.setVolume(0.5);
+  backgroundNoise.play();
 }
 
 function draw() 
 {
-  background(255);
-  DrawSign();
-  CheckMove_P1();
-  CheckMove_P2();
+   background(255);
+  DrawFrame();
+  //Start
+  if(gameMode == 0)
+  {
+    DrawSign();
+  }
+  
+  
+  //Main Game
   if(gameMode == 1)
   {
-    
-    
+    CheckMove_P1();
+    CheckMove_P2();
     DrawBallTrail();
     P1_Paddle.DrawPaddle();
     P2_Paddle.DrawPaddle();
     theBall.draw();
+    
+    //Change Game Timer
+    if(adjustedTimer > 0)
+    {
+      timer++;
+    }
+    if(timer == 60)
+    {
+      adjustedTimer--;
+      timer = 0;
+    }
+    if(adjustedTimer == 0)
+    {
+      gameMode = 2;
+      timer = 0;
+    }
+    
+    //Move Particles
+    for(var partNum = 0; partNum < particles.length; partNum++)
+    {
+      //Initialize particle
+      if(!particles[partNum].init)
+      {
+        particles[partNum].Initialize();
+        particles[partNum].init = true;
+      }
+      
+      particles[partNum].MoveParticle();
+    }
+
+    //Move Bullets
+    for(var bulletNum = 0; bulletNum < bullets.length; bulletNum++)
+    {
+      bullets[bulletNum].move();
+      
+     //reassign bullet indexes
+     for(var p = 0; p < bullets.length; p++)
+     {
+       bullets[p].index = p;
+     }  
+     
+    }
+
+
+    //draw walls
+    if(P1_Wall != null)
+    {
+      P1_Wall.draw();
+    }
+
+    if(P2_Wall != null)
+    {
+      P2_Wall.draw();
+    }
+
   }
   
+  //Game Ending
+  if(gameMode == 2)
+  {
+    DrawBallTrail();
+    fill(0);
+    textSize(48);
+    text("Nice Painting!", 190, 195);
+    timer++;
+    CheckPlayAgain();
+    if(timer >= 180)
+    {
+      gameNoise.setVolume(0.1);
+      DrawEnd();
+      if(!pixelCounted)
+      {
+        CountPixels();
+      }  
+    }
+  }
+  DrawTimer();
+}
+
+//Draws te end Screen
+function DrawEnd()
+{ 
+    line(320, 120, 200, 150);
+    line(320, 120, 440, 150);
+    fill(255, 255, 0);
+    ellipse(320, 120, 5, 5);
+    fill(255, 250, 211);
+    rect(200, 150, 240, 160);
+
+    fill(255, 250, 230);
+    rect(230, 165, 180, 40);
   
+    fill(0);
+    textSize(24);
+    if(redPixels > bluePixels)
+    {
+      text("Red Wins!", 265, 195);
+    }
+    if(bluePixels > redPixels)
+    {
+      text("Blue Wins!", 265, 195); 
+    }
+    if(redPixels == bluePixels)
+    {
+      text("Draw!", 240, 195); 
+    }
+    
+  
+    fill(0);
+    textSize(14);
+    text("P1 covered " + ((redPixels/(height*width))*100).toPrecision(3) +  "% of the canvas.", 210, 230);
+    text("P2 covered " + ((bluePixels/(height*width))*100).toPrecision(3) + "% of the canvas.", 210, 250);
+  
+    stroke(0);
+    fill(255,0,0);
+    rect(270, 270, 100, 30);
+    fill(255);
+    textSize(18);
+    text("Play Again", 280, 293);
+}
+
+//Draws the Game's timer
+function DrawTimer()
+{
+  textSize(18);
+  fill(255);
+  text("Time Left: " + adjustedTimer, 500, 20); 
   
 }
 
-//Draws the sign of the game
-function DrawSign() 
+function DrawFrame()
 {
   //photo trim
   fill(163, 121, 53);
@@ -54,7 +228,11 @@ function DrawSign()
   line(640, 0, 610, 30);
   line(0, 480, 30, 450);
   line(640, 480, 610, 450);
+}
 
+//Draws the sign of the game
+function DrawSign() 
+{
   //Start Sign
   if (gameMode == 0) {
     line(320, 120, 200, 150);
@@ -71,6 +249,9 @@ function DrawSign()
     textSize(24);
     text("Painterly Pong", 240, 195);
     
+    textSize(14);
+    text("P1: Use W & S to move, D to shoot.", 210, 230);
+    text("P2: Use I & K to move, J to shoot.", 210, 250);
     
     //Check if mouse is over play button
     if(mouseX < 350 && mouseX > 290)
@@ -78,12 +259,18 @@ function DrawSign()
       if(mouseY < 300 && mouseY > 260)
       {
         noStroke();
-        fill(255);
+        fill(255,255,0);
         rect(285, 265, 70, 40); 
         if(mouseIsPressed)
         {
+          gameEnd.play();
           gameMode = 1;
-          print(gameMode);
+          adjustedTimer = 60;
+          paddleNoise.play();
+          backgroundNoise.stop();
+          gameNoise.setLoop(true);
+          gameNoise.setVolume(0.5);
+          gameNoise.play();
         }
       }
     }
@@ -96,16 +283,82 @@ function DrawSign()
   }
 }
 
+//Check Play Again
+function CheckPlayAgain()
+{
+    //Check if mouse is over play button
+    if(mouseX < 350 && mouseX > 290)
+    {
+      if(mouseY < 300 && mouseY > 260)
+      {
+        noStroke();
+        fill(255,255,0);
+        rect(285, 265, 70, 40); 
+        stroke(0);
+        if(mouseIsPressed)
+        {
+          gameMode = 1;
+          adjustedTimer = 60;
+          timer = 0;
+
+          //reset the ball
+          theBall.reset();
+
+          gameNoise.setVolume(0.5);
+
+          for(var iball = ballX.length; iball >= 0; iball--)
+          {
+            ballX.splice(iball,1);
+            ballY.splice(iball,1);
+            ballSize.splice(iball,1);
+            ballColor.splice(iball,1);
+          }
+          
+        }
+      }
+    } 
+}
+
+//Shoot Listener
+function keyTyped()
+{
+  if(key == 'd' && gameMode == 1)
+  {
+    paddleNoise.play();
+    bullets[bullets.length] = new Bullet(P1_Paddle.x, P1_Paddle.y, 1, bullets.length);
+  }
+
+  if(key == 'j' && gameMode == 1)
+  {
+    paddleNoise.play();
+    bullets[bullets.length] = new Bullet(P2_Paddle.x, P2_Paddle.y, 2, bullets.length);
+  }
+  /*
+  if(key == 'a' && gameMode == 1)
+  {
+    paddleNoise.play();
+    P1_Wall = new Wall(P1_Paddle.x, P1_Paddle.y, 1);
+  }
+
+  if(key == 'l' && gameMode == 1)
+  {
+    paddleNoise.play();
+    P2_Wall = new Wall(P2_Paddle.x, P2_Paddle.y, 2);
+  }
+  */
+}
+
+
 //Key Press Listener
 //These functions have a problem where only 1 player can move, need to find a fix for this.
 function CheckMove_P1()
 {
-  if(keyIsPressed && key == 'a')
+  if(keyIsPressed && key == 'w')
   {
     keys[0] = true;
     keys[1] = false;
   }
-  if(keyIsPressed && key == 'd')
+  if(keyIsPressed && key == 's')
   {
     keys[1] = true;
     keys[0] = false
@@ -135,14 +388,14 @@ function CheckMove_P1()
 //Player 2 key press
 function CheckMove_P2()
 {
-  if(keyIsPressed && key == 'j')
+  if(keyIsPressed && key == 'i')
   {
     keys[2] = true;
     keys[3] = false;
   }
 
   
-  if(keyIsPressed && key == 'l')
+  if(keyIsPressed && key == 'k')
   {
     keys[3] = true; 
     keys[2] = false;
@@ -171,18 +424,20 @@ function CheckMove_P2()
   }  
 }
 
-
+//Initializes the paddle objects
 function SetPaddles()
 {
    P1_Paddle = new Paddle(70, 30, 1);
    P2_Paddle = new Paddle(570, 30, 2);
 }
 
+//Initializes the ball object
 function SetBall()
 {
    theBall = new Ball(-1, -1);
 }
 
+//This draws the trail the ball leaves
 function DrawBallTrail()
 {
   for(var i = 0; i < ballX.length; i++)
@@ -191,20 +446,21 @@ function DrawBallTrail()
     if(ballColor[i] == 1)
     {
       noStroke();
-      fill(255,150,150);
-      ellipse(ballX[i], ballY[i],15,15);
+      fill(255,100,100);
+      ellipse(ballX[i], ballY[i],ballSize[i],ballSize[i]);
       stroke(0);
     }
     if(ballColor[i] == 2)
     {
       noStroke();
-      fill(150,150,255);
-      ellipse(ballX[i], ballY[i],15,15);
+      fill(100,100,255);
+      ellipse(ballX[i], ballY[i],ballSize[i],ballSize[i]);
       stroke(0);      
     }
   }
 }
 
+//The ball class
 function Ball(x,y)
 {
   this.x = 320;
@@ -217,6 +473,23 @@ function Ball(x,y)
   this.controller = 0;
   this.counter = 0;
   
+  this.size = 15;
+
+  this.reset = function()
+  {
+      this.x = 320;
+      this.y = 240;
+  
+      this.xVel = x;
+      this.yVel = y;
+  
+      this.speed = 1;
+      this.controller = 0;
+      this.counter = 0;
+  
+      this.size = 15;
+  }
+  
   this.draw = function()
   {
     this.counter++;
@@ -224,17 +497,43 @@ function Ball(x,y)
     this.y += this.yVel * this.speed;
     
     //Edge Detect Y axis
-    if(this.y < 30 || this.y > 450)
+    if(this.y < 38 || this.y > 442)
     {
       this.yVel *= -1; 
+      this.size+=2;
+      this.speed+=0.25;
+      paddleNoise.play();
     }
     
     //Edge Detect X axis
-    if(this.x < 30 || this.x > 610)
+    if(this.x < 38)
     {
+      //Create Particles
+      particles[particles.length] = new HitParticles(this.x, this.y, this.size, 5, 2, false);
+      
+      //Reset the ball
       this.x = 320;
       this.xVel *= -1;  
       this.speed = 1;
+      this.size = 15;
+      this.controller = 2;
+      boomNoise.play();
+      
+      
+    }
+    if(this.x > 602)
+    {
+      //Create Particles
+      particles[particles.length] = new HitParticles(this.x, this.y, this.size, 5, 1, false);
+      
+      //Reset the ball
+      this.x = 320;
+      this.xVel *= -1;  
+      this.speed = 1;
+      this.size = 15;
+      this.controller = 1;
+      boomNoise.play();
+      
     }
     
     //Paddle Detect P1
@@ -246,6 +545,8 @@ function Ball(x,y)
         this.xVel *= -1;
         //this.yVel *= -1;
         this.controller = 1;
+        this.size+=2;
+        paddleNoise.play();
       }
     }
     //Paddle Detect P2
@@ -257,8 +558,43 @@ function Ball(x,y)
         this.xVel *= -1;
         //this.yVel *= -1;
         this.controller = 2;
+        this.size+=2;
+        paddleNoise.play();
       }
     }
+
+    /*
+    //Wall detect p1
+    if(P1_Wall != null && this.x-15 > P1_Wall.x + 20 && this.x+15 < P1_Wall.x)
+    {
+      if(this.y-15 > P1_Wall.y + 80 && this.y+15 > P1_Wall.y)
+      {
+        this.speed+=0.25;
+        this.xVel *= -1;
+        //this.yVel *= -1;
+        this.controller = 1;
+        this.size+=2;
+        paddleNoise.play();
+        P1_Wall = null;        
+      }
+    } 
+    //qall Detect P2
+    if(P2_Wall != null && this.x > P2_Wall.x && this.x < P2_Wall.x + 20)
+    {
+      if(this.y > P2_Wall.y && this.y < P2_Wall.y + 80)
+      {
+        this.speed+=0.25;
+        this.xVel *= -1;
+        //this.yVel *= -1;
+        this.controller = 2;
+        this.size+=2;
+        paddleNoise.play();
+        P2_Wall = null
+      }
+    }
+    */
+
+
     if(this.controller == 0)
     {
       fill(255);
@@ -272,22 +608,20 @@ function Ball(x,y)
       fill(0,0,255); 
     }
     
-    ellipse(this.x, this.y, 15, 15);
+    ellipse(this.x, this.y, this.size, this.size);
     
-    if(this.counter == 7)
+    if(this.counter == 5)
     {
       this.counter = 0;
       ballX[ballX.length] = this.x;
       ballY[ballY.length] = this.y;
       ballColor[ballColor.length] = this.controller;  
-    }
-
-    
-    
-    
+      ballSize[ballSize.length] = this.size;
+    }    
   }
 }
 
+//The paddle class
 function Paddle(x, y, playerNum)
 {
   this.x = x;
@@ -307,4 +641,243 @@ function Paddle(x, y, playerNum)
       rect(this.x,this.y,20,80);
     }
   }
+}
+
+//Class responsible for generating particle systems
+function HitParticles(x,y, size, numParticles, controller, init)
+{
+  //particle index
+  this.init = init;
+  
+  //Particle Origin
+  this.x = x;
+  this.y = y;
+  
+  //Size of the particles
+  this.size = size;
+  
+  //Number of Particles
+  this.numPart = numParticles;
+  
+  //Controller of the particles
+  this.controller = controller;
+  
+  //This will hold the variables for particles.
+  this.partX = [];
+  this.partY = [];
+  this.partVelX = [];
+  this.partVelY = [];
+  
+  this.counter = 0;
+  
+  this.Initialize = function()
+  {
+    for(var i = 0; i < this.numPart; i++)
+    {
+      this.partX[i] = this.x;
+      this.partY[i] = this.y;
+      //P1, Right
+      if(this.controller === 1)
+      {
+        this.partVelX[i] = -random(1, 2);
+      }
+      //P2, Left
+      if(this.controller === 2)
+      {
+        this.partVelX[i] = random(1, 2); 
+      }      
+      this.partVelY[i] = -random(0.1, 0.5);
+    }
+  }
+  
+  //This will move the particle
+  this.MoveParticle = function()
+  {
+    
+    this.counter++;
+    
+    for(var j = 0; j < this.partX.length; j++)
+    {
+       if(this.controller == 1)
+       {
+          fill(255,0,0); 
+          for(var part1 = 0; part1 < this.partX.length; part1++)
+          {
+            if(this.partY[part1] < 442 && this.partX[part1] > 38)
+            {
+              ellipse(this.partX[part1], this.partY[part1], this.size, this.size);
+              this.partX[part1] += this.partVelX[part1];
+              this.partY[part1] += this.partVelY[part1];
+              this.partVelY[part1]+= 0.005;
+            
+              if(this.counter == 5)
+              {     
+                ballX[ballX.length] = this.partX[part1];
+                ballY[ballY.length] = this.partY[part1];
+                ballColor[ballColor.length] = 1;  
+                ballSize[ballSize.length] = this.size;
+              }
+              
+            }
+            
+            
+          }
+       }
+    
+      
+      if(this.controller == 2)
+      {
+          fill(0,0,255); 
+          for(var part2 = 0; part2 < this.partX.length; part2++)
+          {
+            if(this.partY[part2] < 442 && this.partX[part2] < 602)
+            {
+              ellipse(this.partX[part2], this.partY[part2], this.size, this.size);
+              this.partX[part2] += this.partVelX[part2];
+              this.partY[part2] += this.partVelY[part2];
+              this.partVelY[part2]+= 0.005;
+            
+              if(this.counter == 5)
+              {
+                ballX[ballX.length] = this.partX[part2];
+                ballY[ballY.length] = this.partY[part2];
+                ballColor[ballColor.length] = 2;  
+                ballSize[ballSize.length] = this.size;  
+              }
+      
+            }
+
+          }
+      }
+      
+      //reset the counter
+      if(this.counter == 5)
+      {
+        this.counter = 0;
+      }
+      
+    }
+  }
+  
+}
+
+//This is the bullet class
+function Bullet(x, y, controller, index)
+{
+  this.x = x;
+  this.y = y + 40;
+  this.index = index;
+
+  this.drawing = false;
+
+  this.controller = controller;
+
+  
+  this.move = function()
+  {
+
+    //red
+    if(this.controller == 1)
+    {
+      this.x += 5;
+      fill(255,0,0);    
+    }
+
+    //blue
+    if(this.controller == 2)
+    {
+      this.x -= 5;
+      fill(0,0,255);
+    }
+    rect(this.x - 10, this.y - 10, 10, 10);
+    //Check Collision
+
+
+    //Wall collision
+    if(this.x < 38 || this.x > 602)
+    {
+      bullets.splice(this.index, 1);
+    }
+
+    //Ball collision
+    if(this.x > theBall.x - theBall.size/2 && this.x < theBall.x + theBall.size/2)
+    {
+      if(this.y > theBall.y - theBall.size/2 && this.y < theBall.x + theBall.size/2)
+      {
+        bullets.splice(this.index, 1);
+
+        boomNoise.setVolume(0.55);
+        boomNoise.play();
+        theBall.controller = this.controller;
+        theBall.size += 0.5;
+        ballX[ballX.length] = this.x - 10;
+        ballY[ballY.length] = this.y - 10;
+        ballColor[ballColor.length] = this.controller;  
+        ballSize[ballSize.length] = 50; 
+      }
+    }   
+  }
+}
+
+function Wall(x,y,controller)
+{
+  this.x = x;
+  this.y = y;
+  this.controller = controller;
+
+  this.life = 5;
+
+  this.draw = function()
+  {
+    if(this.controller == 1)
+    {
+      fill(255,0,0);
+      rect(this.x + 35, this.y, 20, 80);
+    }
+    if(this.controller == 2)
+    {
+      fill(0,0,255);
+      rect(this.x - 35, this.y, 20, 80);
+    }
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+//Access the pixel array to count how many red and blue pixels there are
+function CountPixels()
+{
+  pixelCounted = true;
+  loadPixels();
+  redPixels = 0;
+  bluePixels = 0;
+  //loop though the pixel array
+  for(var x = 0; x < height; x++)
+  {
+    for(var y = 0; y < width; y++)
+    {
+      var index = (x + y * width) * 4;
+      //Check for red
+      if(pixels[index] === 255 && pixels[index+1] === 100 && pixels[index+2] === 100)
+      {
+        redPixels++;
+      }
+      
+      //Check for blue
+      if(pixels[index] === 100 && pixels[index+1] === 100 && pixels[index+2] === 255)
+      {
+        bluePixels++;
+      }
+    }
+  } 
+  //print(redPixels + " " + bluePixels);
+  updatePixels();
 }
